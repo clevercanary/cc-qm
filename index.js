68c8bf83ae9553606ef592043838af6cd2524572
cdbajorin <chris@clevercanary.com>
2015-09-28 15:14:29 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+var QueryModel = require("./lib/querymodel");
+var queryBuilder = require("./lib/querybuilder");
+
+module.exports = {
+    QueryModel: QueryModel,
+    queryBuilder: queryBuilder
+};
