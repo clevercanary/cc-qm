dac596501f5c9b7bef92464abf106fa1b803fcd8
cdbajorin <chris@clevercanary.com>
2016-05-24 11:29:45 -0400

    added options to regex queryfilter

--- lib/cc-qm.d.ts
+++ lib/cc-qm.d.ts
@@ -23,83 +23,115 @@ interface RequestParams {
         sorts: string[];
         populates: string[]
         listFilters: any[];
-        fn: {name:string};
+        fn: {name: string};
         offset: number;
         limit: number;
         distinct: string;
     }
 }
 
+type RegexOptions = "i"|"m"|"im"|"mi"
+
 export class QueryModel {
-
-    constructor(RequestParams?)
-
-    filters: any[];
-    sorts: string[];
-    populates: string[];
-    listFilters: any[];
-    fn: {name:string};
-    offset: number;
-    limit: number;
-    paginated: boolean;
-    sortable: boolean;
-
-    isHasFunction():boolean;
-    setPaginated(paginated:boolean):void;
-    isPaginated():boolean;
-    clearPagination():void;
-    setSortable(sortable:boolean):void;
-    isSortable():boolean;
-    initFrom(QueryModel):QueryModel
-
+    
+    constructor(req?: RequestParams)
+    
+    isHasFunction(): boolean;
+    
+    setPaginated(paginated: boolean): void;
+    
+    isPaginated(): boolean;
+    
+    clearPagination(): void;
+    
+    setSortable(sortable: boolean): void;
+    
+    isSortable(): boolean;
+    
+    initFrom(qm: QueryModel): QueryModel
+    
     /**
      * Add filters
      */
-    addEqualsQueryFilter(fieldName:string, value:any):void;
-    addInQueryFilter(fieldName:string, value:any):void;
-    addNotQueryFilter(fieldName:string, value:any):void;
-    addOrQueryFilter(conditions:QueryFilter[]):void;
-    addAndQueryFilter(conditions:QueryFilter[]):void;
-    addGtQueryFilter(fieldName:string, value:any):void;
-    addLtQueryFilter(fieldName:string, value:any):void;
-    addRegexpQueryFilter(fieldName:string, value:string):void;
-    addMatchQueryFilter(fieldName:string, value:any):void;
-    addSizeQueryFilter(fieldName:string, value:any):void;
-
+    addEqualsQueryFilter(fieldName: string, value: any): void;
+    
+    addInQueryFilter(fieldName: string, value: any): void;
+    
+    addNotQueryFilter(fieldName: string, value: any): void;
+    
+    addOrQueryFilter(conditions: QueryFilter[]): void;
+    
+    addAndQueryFilter(conditions: QueryFilter[]): void;
+    
+    addGtQueryFilter(fieldName: string, value: any): void;
+    
+    addLtQueryFilter(fieldName: string, value: any): void;
+    
+    addRegexpQueryFilter(fieldName: string, value: string, options?: string): void;
+    
+    addMatchQueryFilter(fieldName: string, value: any): void;
+    
+    addSizeQueryFilter(fieldName: string, value: any): void;
+    
     /**
      * Build Filters
      */
-    buildEqualsQueryFilter(fieldName:string, value:any):QueryFilter;
-    buildInQueryFilter(fieldName:string, value:any):QueryFilter;
-    buildNotQueryFilter(fieldName:string, value:any):QueryFilter;
-    buildGtQueryFilter(fieldName:string, value:any):QueryFilter;
-    buildLtQueryFilter(fieldName:string, value:any):QueryFilter;
-    buildSizeQueryFilter(fieldName:string, value:any):QueryFilter;
-    buildMatchQueryFilter(fieldName:string, value:any):QueryFilter;
-    buildAndQueryFilter(conditions:QueryFilter[]):ConditionalFilter;
-    buildOrQueryFilter(conditions:QueryFilter[]):ConditionalFilter;
-
+    buildEqualsQueryFilter(fieldName: string, value: any): QueryFilter;
+    
+    buildInQueryFilter(fieldName: string, value: any): QueryFilter;
+    
+    buildNotQueryFilter(fieldName: string, value: any): QueryFilter;
+    
+    buildGtQueryFilter(fieldName: string, value: any): QueryFilter;
+    
+    buildLtQueryFilter(fieldName: string, value: any): QueryFilter;
+    
+    buildSizeQueryFilter(fieldName: string, value: any): QueryFilter;
+    
+    buildMatchQueryFilter(fieldName: string, value: any): QueryFilter;
+    
+    buildAndQueryFilter(conditions: QueryFilter[]): ConditionalFilter;
+    
+    buildOrQueryFilter(conditions: QueryFilter[]): ConditionalFilter;
+    
     /**
      * Get/Set Operators
      */
-    addPopulates(fieldName:string, childrenFieldNames?:string[]):void;
-    addSort(fieldName: string, asc:boolean):void;
-    addSelects(fields:string):void;
-    getFilters():QueryFilter[];
-    getListFilters():string[];
-    getFn():{ name:string };
-    getLimit():number;
-    getOffset():number;
-    getPopulates():string[];
-    getSorts():string[];
-    getSelects():string;
-    getLean():boolean;
-    setAsCountFunction():void;
-    getDistinct():string;
-    setDistinct(key:string):void;
-    setLimit(limit:number):void;
-    setOffset(offset:number):void;
-    setLean(lean:boolean):void;
+    addPopulates(fieldName: string, childrenFieldNames?: string[]): void;
+    
+    addSort(fieldName: string, asc: boolean): void;
+    
+    addSelects(fields: string): void;
+    
+    getFilters(): QueryFilter[];
+    
+    getListFilters(): string[];
+    
+    getFn(): { name: string };
+    
+    getLimit(): number;
+    
+    getOffset(): number;
+    
+    getPopulates(): string[];
+    
+    getSorts(): string[];
+    
+    getSelects(): string;
+    
+    getLean(): boolean;
+    
+    setAsCountFunction(): void;
+    
+    getDistinct(): string;
+    
+    setDistinct(key: string): void;
+    
+    setLimit(limit: number): void;
+    
+    setOffset(offset: number): void;
+    
+    setLean(lean: boolean): void;
 }
 
 
@@ -114,19 +146,19 @@ interface Populate {
 }
 
 interface Paging {
-    offset:number;
-    limit:number;
+    offset: number;
+    limit: number;
 }
 
 interface QueryBuilder {
-    buildQuery<T>(query:Query<T>, qm:QueryModel, listConfig?:Object):void;
-    addFilters<T>(query:Query<T>, filters:Filter[]):void;
-    addSortOrder<T>(query:Query<T>, sorts:Sort[]):void;
-    addPopulates<T>(query:Query<T>, populates:Populate):void;
-    addPaging<T>(query:Query<T>, paging:Paging):void;
-    addFunction<T>(query:Query<T>, qm:QueryModel, listConfig):void;
-    addSelects<T>(query:Query<T>, selects:string):void;
-    addLean<T>(query:Query<T>, lean:boolean):void;
+    buildQuery<T>(query: Query<T>, qm: QueryModel, listConfig?: Object): void;
+    addFilters<T>(query: Query<T>, filters: Filter[]): void;
+    addSortOrder<T>(query: Query<T>, sorts: Sort[]): void;
+    addPopulates<T>(query: Query<T>, populates: Populate): void;
+    addPaging<T>(query: Query<T>, paging: Paging): void;
+    addFunction<T>(query: Query<T>, qm: QueryModel, listConfig): void;
+    addSelects<T>(query: Query<T>, selects: string): void;
+    addLean<T>(query: Query<T>, lean: boolean): void;
 }
 export var queryBuilder: QueryBuilder;
 
